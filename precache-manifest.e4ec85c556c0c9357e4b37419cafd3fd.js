d1c2139209945f5bc0734fe2becdf286dfe6015e
piyu0809 <phirema1@binghamton.edu>
2019-02-14 01:25:54 -0500

    Updates

---
+++ precache-manifest.e4ec85c556c0c9357e4b37419cafd3fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea51ecad8541e8dcd35e",
+    "url": "/portfolio/static/css/main.a0c36350.chunk.css"
+  },
+  {
+    "revision": "ea51ecad8541e8dcd35e",
+    "url": "/portfolio/static/js/main.ea51ecad.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/portfolio/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d178fdaa0272f31065b28ffa1faca9d9",
+    "url": "/portfolio/index.html"
+  }
+];
