8150342f6a0d1c999d29aa6eca21eb008a079d27
piyu0809 <phirema1@binghamton.edu>
2019-02-14 02:18:19 -0500

    Updates

---
+++ precache-manifest.95b577069857f3fa153fde9b43da61cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80ee4e2fab08f73f7fe5",
+    "url": "/portfolio/static/css/main.a05fbdca.chunk.css"
+  },
+  {
+    "revision": "80ee4e2fab08f73f7fe5",
+    "url": "/portfolio/static/js/main.80ee4e2f.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/portfolio/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "7ddbe579db13f96e901d275b29faaa89",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.97ff39802b727e28e69228383389422b.js"
+  "/portfolio/precache-manifest.95b577069857f3fa153fde9b43da61cc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/me/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
