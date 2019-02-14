e34f5e8dd3c070d2a2b59160f385d394567800b9
piyu0809 <phirema1@binghamton.edu>
2019-02-14 02:01:15 -0500

    Updates

---
+++ precache-manifest.3905323f801f939911ec15beed96d7ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ad10e728bdfaf7157b",
+    "url": "/portfolio/static/css/main.a05fbdca.chunk.css"
+  },
+  {
+    "revision": "26ad10e728bdfaf7157b",
+    "url": "/portfolio/static/js/main.26ad10e7.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/portfolio/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "8cbfa352788af8db0df29ae36dfffc64",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e4ec85c556c0c9357e4b37419cafd3fd.js"
+  "/portfolio/precache-manifest.3905323f801f939911ec15beed96d7ec.js"
 );
 
 workbox.clientsClaim();
